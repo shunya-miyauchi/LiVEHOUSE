a440fad3c0f48b1ce11d1c5fd8fbd117c55a1ad1
Miyauchi <hy.miyauchi@gmail.com>
2022-04-04 13:33:25 +0900

    [add]livehouse_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# livehouse
+Livehouse.create!(
+  name: "下北沢BASEMENT BAR",
+  address: "東京都世田谷区代沢5-18-1 カラバッシュビルB1F",
+  url: "https://toos.co.jp/basementbar/",
+  nearest_station: "下北沢駅より徒歩10分",
+)
+
+Livehouse.create!(
+  name: "Spotify O-nest",
+  address: "東京都渋谷区円山町2-3 6F",
+  url: "https://shibuya-o.com/nest/",
+  nearest_station: "渋谷駅より徒歩5分",
+)
