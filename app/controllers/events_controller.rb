1b5689f206f22dc72fb54e23ad18b9ae24024017
Miyauchi <hy.miyauchi@gmail.com>
2022-04-05 19:23:18 +0900

    [add]ライブハウスごとの情報参照、画面切替

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,8 @@
 class EventsController < ApplicationController
+
+  def index
+    @livehouses = Livehouse.all
+    @livehouse = Livehouse.find(params[:livehouse_id])
+    @events = @livehouse.events
+  end
 end
