5785bf97ce60934692d5c0ce84a284cd3f560082
Miyauchi <hy.miyauchi@gmail.com>
2022-04-05 19:18:33 +0900

    [add]routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :livehouses do
+    resources :events, only: %i(index show)
+  end
 end
