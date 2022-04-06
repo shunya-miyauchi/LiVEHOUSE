ddcb88baa6c09a77362055fff23994c18adf78df
Miyauchi <hy.miyauchi@gmail.com>
2022-04-06 19:02:18 +0900

    [add]Userモデル作成(devise使用)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'livehouses#index'
   resources :livehouses do
     resources :events, only: %i[index show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_404_113_723) do
+ActiveRecord::Schema.define(version: 2022_04_06_061439) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'events', force: :cascade do |t|
-    t.string 'title', null: false
-    t.date 'held_on', null: false
-    t.string 'open', default: '未定'
-    t.string 'start', default: '未定'
-    t.integer 'price'
-    t.string 'artist', default: '未定'
-    t.bigint 'livehouse_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.text 'url'
-    t.index ['held_on'], name: 'index_events_on_held_on'
-    t.index ['livehouse_id'], name: 'index_events_on_livehouse_id'
-    t.index %w[title held_on start], name: 'for_upsert', unique: true
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.date "held_on", null: false
+    t.string "open", default: "未定"
+    t.string "start", default: "未定"
+    t.integer "price"
+    t.string "artist", default: "未定"
+    t.bigint "livehouse_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "url"
+    t.index ["held_on"], name: "index_events_on_held_on"
+    t.index ["livehouse_id"], name: "index_events_on_livehouse_id"
+    t.index ["title", "held_on", "start"], name: "for_upsert", unique: true
   end
 
-  create_table 'livehouses', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'address', null: false
-    t.text 'url', null: false
-    t.string 'nearest_station'
-    t.float 'latitude'
-    t.float 'longitude'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "livehouses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.text "url", null: false
+    t.string "nearest_station"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'events', 'livehouses'
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "display_name", null: false
+    t.text "image"
+    t.boolean "admin", default: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "events", "livehouses"
 end
