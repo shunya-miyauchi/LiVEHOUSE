1c46cfeee0fdcb1b35712e761bc22fa8c01da3c6
Miyauchi <hy.miyauchi@gmail.com>
2022-04-08 20:32:39 +0900

    [add]userスペック追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'たかはし' }
+    display_name { 'takahashi' }
+    email { 'aaa@gmail.com' }
+    password { "123456" }
+    password_confirmation { "123456"}
+  end
+  factory :user_second,  class: User do
+    name { 'やまもと' }
+    display_name { 'yamamoto' }
+    email { 'bbb@gmail.com' }
+    password { "1234567" }
+    password_confirmation { "1234567"}
   end
 end
