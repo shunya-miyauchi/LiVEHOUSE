115bd75bc5fce5100ccb53d3c7306b23ef48e772
Miyauchi <hy.miyauchi@gmail.com>
2022-04-10 00:04:34 +0900

    [fix]ストロングパラメータ記述場所変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name display_name image admin])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name display_name image admin])
-  end
 end
