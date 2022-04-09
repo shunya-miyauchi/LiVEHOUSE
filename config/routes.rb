2de594d7f3a39c735553d377287cc6dc806bcd78
Miyauchi <hy.miyauchi@gmail.com>
2022-04-09 22:37:42 +0900

    [fix]修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
-  root 'livehouses#index'
   resources :livehouses do
     resources :events, only: %i[index show]
   end
-  resources :user, only: %i[show]
+  resources :users, only: %i[show]
+  root 'livehouses#index'
 end
