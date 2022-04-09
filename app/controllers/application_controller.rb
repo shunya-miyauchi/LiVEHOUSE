f922d96ea89830cc4b2d134b29fdbbb8ff590686
Miyauchi <hy.miyauchi@gmail.com>
2022-04-09 23:18:12 +0900

    [fix]rubocop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name display_name image admin])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name display_name image admin])
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations"
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   resources :livehouses do
     resources :events, only: %i[index show]
