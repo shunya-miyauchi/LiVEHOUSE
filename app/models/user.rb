14b9f5279c8f9a93219e849661654cd7d6d3ded3
Miyauchi <hy.miyauchi@gmail.com>
2022-04-10 15:31:30 +0900

    [add]favoritesテーブル-アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_livehouses, through: :favorites, source: :livehouses
   mount_uploader :image, IconUploader
 end
