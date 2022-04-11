5d031c4a93721f1dbcc15b0bc3511768b66be1cc
Miyauchi <hy.miyauchi@gmail.com>
2022-04-11 14:47:42 +0900

    [add]commentsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_060840) do
+ActiveRecord::Schema.define(version: 2022_04_11_054351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
@@ -72,6 +82,8 @@ ActiveRecord::Schema.define(version: 2022_04_10_060840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "events"
+  add_foreign_key "comments", "users"
   add_foreign_key "events", "livehouses"
   add_foreign_key "favorites", "livehouses"
   add_foreign_key "favorites", "users"
