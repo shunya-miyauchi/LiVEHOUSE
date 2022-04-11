c30326a52e38e77b162e069a92cf8bd21930342e
Miyauchi <hy.miyauchi@gmail.com>
2022-04-11 12:11:50 +0900

    [add]お気に入りページ追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,14 @@
 class FavoritesController < ApplicationController
-  before_action :livehouse_params
+  before_action :livehouse_params, only: %i[create destroy]
+
+  def index
+    @favorite_livehouses = current_user.favorite_livehouses
+  end
+
+  def show
+    @livehouse = Livehouse.find(params[:id])
+    @events = @livehouse.events.where('held_on >= ?', Date.current)
+  end
 
   def create
     current_user.favorites.create(livehouse_id: params[:id])
