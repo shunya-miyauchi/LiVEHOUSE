b46bd0223a636fa01f7aa0d7c07de4b00ef75ea5
Miyauchi <hy.miyauchi@gmail.com>
2022-04-11 14:48:35 +0900

    [add]commentsアソシエーション設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :livehouse
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :favorite_livehouses, through: :favorites, source: :livehouse
+  has_many :comments, dependent: :destroy
+  
   mount_uploader :image, IconUploader
 
   def favorites_by?(livehouse_id)
