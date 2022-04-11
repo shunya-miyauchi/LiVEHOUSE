4ac9c28fbfc137336a5b4835cdc14bdc264d143c
Miyauchi <hy.miyauchi@gmail.com>
2022-04-11 19:47:26 +0900

    [add]comments/create追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    @livehouse = Livehouse.find(params[:livehouse_id])
+    @event = Event.find(params[:event_id])
+    @comment = @event.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    render :index
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
   end
 end
