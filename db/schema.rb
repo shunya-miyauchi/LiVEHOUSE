d792ba3fc380b56539fa99715f9bd9406cf9ed89
Miyauchi <hy.miyauchi@gmail.com>
2022-04-12 12:45:16 +0900

    [add]joinsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_054351) do
+ActiveRecord::Schema.define(version: 2022_04_12_034323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2022_04_11_054351) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "joins", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_joins_on_event_id"
+    t.index ["user_id"], name: "index_joins_on_user_id"
+  end
+
   create_table "livehouses", force: :cascade do |t|
     t.string "name", null: false
     t.string "address", null: false
@@ -87,4 +96,6 @@ ActiveRecord::Schema.define(version: 2022_04_11_054351) do
   add_foreign_key "events", "livehouses"
   add_foreign_key "favorites", "livehouses"
   add_foreign_key "favorites", "users"
+  add_foreign_key "joins", "events"
+  add_foreign_key "joins", "users"
 end
