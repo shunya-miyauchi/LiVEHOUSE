c7f1fe3aa36b8995f0666c360f64bc4e26fbc294
Miyauchi <hy.miyauchi@gmail.com>
2022-04-12 15:03:40 +0900

    [add]マイページ:ライブ予定追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     if current_user.id.to_s == params[:id]
       @user = User.find(params[:id])
+      @events = @user.join_events.where('held_on >= ?', Date.current)
     else
       redirect_to root_path
     end
