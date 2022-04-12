297addb9d3c06c96cb07d2afc796567de470257a
Miyauchi <hy.miyauchi@gmail.com>
2022-04-12 15:30:19 +0900

    [add]イベント参加記録ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :correct_user?
+  before_action :set_user
 
-  def show
-    if current_user.id.to_s == params[:id]
-      @user = User.find(params[:id])
-      @events = @user.join_events.where('held_on >= ?', Date.current)
-    else
-      redirect_to root_path
-    end
+  def show 
+    @events = @user.join_events.where('held_on >= ?', Date.current)
+  end
+
+  def past_joins
+    @events = @user.join_events.where('held_on < ?', Date.current)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def correct_user?
+    current_user.id.to_s == params[:id]
   end
 end
