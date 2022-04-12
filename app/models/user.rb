98665fdf5ea2bc239efda7bc4ee55b3e46db80d6
Miyauchi <hy.miyauchi@gmail.com>
2022-04-12 12:46:30 +0900

    [add]join中間テーブル関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_livehouses, through: :favorites, source: :livehouse
   has_many :comments, dependent: :destroy
+  has_many :events
 
   mount_uploader :image, IconUploader
 
