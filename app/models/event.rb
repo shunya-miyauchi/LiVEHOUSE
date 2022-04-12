1a910dccd05ce487e64e1955021b1a106fececf1
Miyauchi <hy.miyauchi@gmail.com>
2022-04-12 14:42:55 +0900

    [add]Userメソッド追加,joins関連付け修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :livehouse
   has_many :comments, dependent: :destroy
-  has_many :users
+  has_many :joins
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,15 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_livehouses, through: :favorites, source: :livehouse
   has_many :comments, dependent: :destroy
-  has_many :events
+  has_many :joins
 
   mount_uploader :image, IconUploader
 
   def favorites_by?(livehouse_id)
     favorites.find_by(livehouse_id: livehouse_id).present?
+  end
+
+  def joins_by?(event_id)
+    joins.find_by(event_id: event_id).present?
   end
 end
