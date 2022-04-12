0007e36aeefcf2b34f8a676969730aff9a2bf7d1
Miyauchi <hy.miyauchi@gmail.com>
2022-04-12 14:43:38 +0900

    [add]joinsコントローラ作成

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,7 +1,17 @@
 class JoinsController < ApplicationController
+  before_action :event_params
+  
   def create
+    current_user.joins.create(event_id: params[:id])
   end
 
   def destroy
+    current_user.joins.find_by(event_id: params[:id]).destroy
+  end
+
+  private
+
+  def event_params
+    @event = Event.find(params[:id])
   end
 end
