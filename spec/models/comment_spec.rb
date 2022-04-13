8801d7c19d0c014b8b151c7541c6c6716fe4041b
Miyauchi <hy.miyauchi@gmail.com>
2022-04-13 11:05:13 +0900

    [add]test:modelテスト作成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションテスト" do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:livehouse) { FactoryBot.create(:livehouse) }
+    let!(:event) { FactoryBot.create(:event, livehouse: livehouse) }
+    context 'contentカラム' do
+      it '空欄である' do
+        comment = FactoryBot.build(:comment, 
+          user_id: user.id, 
+          event_id: event.id,
+          content: ""
+        )
+        expect(comment).not_to be_valid
+      end
+    end
+  end
 end
--- spec/models/livehouse_spec.rb
+++ spec/models/livehouse_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Livehouse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "アソシエーションテスト" do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:livehouse) { FactoryBot.create(:livehouse) }
+    let!(:event) { FactoryBot.create(:event, livehouse: livehouse) }
+    context 'livehouseが削除された場合' do
+      it '関連付けられたfavoriteも削除される' do
+        FactoryBot.create(:favorite, user_id: user.id, livehouse_id: livehouse.id)
+        expect{livehouse.destroy}.to change {Favorite.count}.by(-1)
+      end
+      it '関連付けられたeventも削除される' do
+        expect{livehouse.destroy}.to change {Event.count}.by(-1)
+      end
+    end
+  end
 end
