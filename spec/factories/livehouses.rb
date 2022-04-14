e4bae624bae343ecaa254441f9b1a3c799cbb9a5
Miyauchi <hy.miyauchi@gmail.com>
2022-04-15 00:15:53 +0900

    [fix]test:factory修正

--- spec/factories/livehouses.rb
+++ spec/factories/livehouses.rb
@@ -3,10 +3,12 @@ FactoryBot.define do
     name { '下北沢BASEMENT BAR' }
     address { '東京都世田谷区代沢5-18-1' }
     url { 'https://toos.co.jp/basementbar/' }
+    place_id { '15' }
   end
   factory :livehouse_second, class: Livehouse do
     name { '心斎橋Pangea' }
     address { '大阪府大阪市中央区西心斎橋2-10-34' }
     url { 'https://livepangea.com/' }
+    place_id { '42' }
   end
 end
