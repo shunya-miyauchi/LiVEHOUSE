7fda84ac9d50c0bd2100e9efb6d7196a9fa67df9
Miyauchi <hy.miyauchi@gmail.com>
2022-04-15 17:06:04 +0900

    [add]model:Event/scope定義

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :livehouse
   has_many :comments, dependent: :destroy
   has_many :joins
+
+  scope :date_after_today, -> { where('held_on >= ?', Date.current) }
+  scope :date_before_today, -> { where('held_on < ?', Date.current) }
+  scope :sort_held_on, -> { order(held_on: "ASC") }
 end
