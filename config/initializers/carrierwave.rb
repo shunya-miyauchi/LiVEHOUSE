e035d92ccd470876ac0b9de7a835afdff29cda3e
Miyauchi <hy.miyauchi@gmail.com>
2022-04-18 15:52:36 +0900

    [update]画像保存先をAWS(S3)へ変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,37 @@
 if Rails.env.test?
-  CarrierWave.configure do |config|
+  config.storage = :file
+  config.enable_processing = false
+end
+
+CarrierWave.configure do |config|
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
+
+  if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
+  else
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region:                'ap-northeast-1',
+        path_style:            true,
+    }
+
+    config.fog_public     = false
+    config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+
+    case Rails.env
+      when 'production'
+        config.fog_directory = 'livehouse-production'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/livehouse-production'
+      when 'development'
+        config.fog_directory = 'livehouse-development'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/livehouse-development'
+    end
   end
 end
