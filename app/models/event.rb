8d986cad2373cde45eda4994643ab1b888ff8fea
Miyauchi <hy.miyauchi@gmail.com>
2022-04-19 09:11:45 +0900

    [add]model:Event/scope追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,9 @@ class Event < ApplicationRecord
   has_many :blogs
 
   scope :date_after_today, -> { where('held_on >= ?', Date.current) }
+  scope :this_week, -> { where('held_on <= ?',Date.current.end_of_week) }
   scope :date_before_today, -> { where('held_on < ?', Date.current) }
+  scope :next_week, -> { where('held_on >= ?',Date.current.next_week(:monday)).where('held_on <= ?',Date.current.next_week(:sunday)) }
   scope :sort_held_on, -> { order(held_on: 'ASC') }
   scope :reverse_held_on, -> { order(held_on: 'DESC') }
 
