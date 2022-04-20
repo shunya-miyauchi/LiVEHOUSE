67497d0f53036637f6c074b2455dc2c523c2e532
Miyauchi <hy.miyauchi@gmail.com>
2022-04-20 10:57:02 +0900

    [fix]model:関連削除

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :livehouse
   has_many :comments, dependent: :destroy
-  has_many :joins
-  has_many :blogs
+  has_many :joins, dependent: :destroy
+  has_many :blogs, dependent: :destroy
 
   scope :date_after_today, -> { where('held_on >= ?', Date.current) }
   scope :this_week, -> { where('held_on <= ?', Date.current.end_of_week) }
