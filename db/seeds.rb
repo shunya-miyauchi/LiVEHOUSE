9e4984fc160fc658a1a1ff8c4eb0810d08970b68
Miyauchi <hy.miyauchi@gmail.com>
2022-04-20 10:51:09 +0900

    [fix]seed:インデント修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,40 @@
 # livehouse
 Livehouse.create!([
-                    {
-                      name: '下北沢BASEMENT BAR',
-                      address: '東京都世田谷区代沢5-18-1 カラバッシュビルB1F',
-                      url: 'https://toos.co.jp/basementbar/',
-                      nearest_station: '下北沢駅より徒歩10分',
-                      place_id: '15'
-                    },
-                    {
-                      name: '下北沢SHELTER',
-                      address: '東京都世田谷区北沢2-6-10仙田ビルB1',
-                      url: 'http://www.loft-prj.co.jp/SHELTER/',
-                      nearest_station: '下北沢駅より徒歩５分',
-                      place_id: '15'
-                    },
-                    {
-                      name: '新代田FEVER',
-                      address: '東京都世田谷区羽根木1-1-14 新代田ビル1F',
-                      url: 'https://www.fever-popo.com/',
-                      nearest_station: '新代田駅より徒歩30秒',
-                      place_id: '15'
-                    },
-                    {
-                      name: 'Spotify O-nest',
-                      address: '東京都渋谷区円山町2-3 6F',
-                      url: 'https://shibuya-o.com/nest/',
-                      nearest_station: '渋谷駅より徒歩5分',
-                      place_id: '14'
-                    },
-                    {
-                      name: 'Live House Pangea',
-                      address: '大阪市中央区西心斎橋2-10-34 心斎橋ウエスト363ビル1F',
-                      url: 'https://livepangea.com/',
-                      nearest_station: '四ツ橋駅より徒歩3分'
-                    }
-                  ])
+  {
+    name: '下北沢BASEMENT BAR',
+    address: '東京都世田谷区代沢5-18-1 カラバッシュビルB1F',
+    url: 'https://toos.co.jp/basementbar/',
+    nearest_station: '下北沢駅より徒歩10分',
+    place_id: '15'
+  },
+  {
+    name: '下北沢SHELTER',
+    address: '東京都世田谷区北沢2-6-10仙田ビルB1',
+    url: 'http://www.loft-prj.co.jp/SHELTER/',
+    nearest_station: '下北沢駅より徒歩５分',
+    place_id: '15'
+  },
+  {
+    name: '新代田FEVER',
+    address: '東京都世田谷区羽根木1-1-14 新代田ビル1F',
+    url: 'https://www.fever-popo.com/',
+    nearest_station: '新代田駅より徒歩30秒',
+    place_id: '15'
+  },
+  {
+    name: 'Spotify O-nest',
+    address: '東京都渋谷区円山町2-3 6F',
+    url: 'https://shibuya-o.com/nest/',
+    nearest_station: '渋谷駅より徒歩5分',
+    place_id: '14'
+  },
+  {
+    name: 'Live House Pangea',
+    address: '大阪市中央区西心斎橋2-10-34 心斎橋ウエスト363ビル1F',
+    url: 'https://livepangea.com/',
+    nearest_station: '四ツ橋駅より徒歩3分'
+  }
+])
 
 # users
 5.times do |n|
