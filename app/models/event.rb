366d508e3d0e7bd317c315b7ce9b541eb515abfe
Miyauchi <hy.miyauchi@gmail.com>
2022-04-21 09:02:11 +0900

    [add]model:scopeメソッド追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,11 +8,12 @@ class Event < ApplicationRecord
   scope :this_week, -> { where('held_on <= ?', Date.current.end_of_week) }
   scope :date_before_today, -> { where('held_on < ?', Date.current) }
   scope :next_week, -> { where('held_on >= ?', Date.current.next_week(:monday)).where('held_on <= ?', Date.current.next_week(:sunday)) }
+  scope :after_next_week, -> { where('held_on >= ?', Date.current.since(2.week).beginning_of_week) }
   scope :sort_held_on, -> { order(held_on: 'ASC') }
   scope :reverse_held_on, -> { order(held_on: 'DESC') }
 
   def date_title
     date = held_on.strftime('%Y/%-m/%d')
-    "#{date}#{title}".truncate(30, omission: '...')
+    "#{date}　#{title}".truncate(30, omission: '...')
   end
 end
