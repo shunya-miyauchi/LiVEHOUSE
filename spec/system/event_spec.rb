693e4561b13f721ff061df9cb2085a074abc59e1
Miyauchi <hy.miyauchi@gmail.com>
2022-04-23 12:01:09 +0900

    [fix]test:イベント期間検索テスト

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -40,6 +40,8 @@ RSpec.describe 'イベント管理', type: :system do
       end
     end
   end
+
+
   describe 'イベント詳細画面' do
     before do
       travel_to Time.zone.local(2022, 1, 3)
@@ -69,6 +71,7 @@ RSpec.describe 'イベント管理', type: :system do
       end
     end
   end
+
 
   describe 'イベントコメント投稿機能' do
     before do
@@ -103,6 +106,94 @@ RSpec.describe 'イベント管理', type: :system do
       end
       it 'コメント投稿フォームが表示されない' do
         expect(page).not_to have_button '投稿'
+      end
+    end
+  end
+
+
+  describe 'イベント期間検索機能' do
+    context 'イベントページ' do
+      before do
+        travel_to Time.zone.local(2022, 1, 3)
+        visit livehouse_events_path(livehouse)
+      end
+      context '左の検索ボックスに日付を入力した場合' do
+        before do
+          fill_in 'q_event[held_on_gteq]', with: '002022-01-10'
+          find(".search_form .search_btn").click 
+        end
+        it 'その日付以降のイベントのみ表示される' do
+          expect(page).not_to have_content 'タイトル１'
+          expect(page).to have_content 'タイトル２'
+          expect(page).to have_content 'タイトル３'
+        end
+      end
+      context '右の検索ボックスに日付を入力した場合' do
+        before do
+          fill_in 'q_event[held_on_lteq]', with: '002022-01-10'
+          find(".search_form .search_btn").click 
+        end
+        it 'その日付以前のイベントのみ表示される' do
+          expect(page).to have_content 'タイトル１'
+          expect(page).to have_content 'タイトル２'
+          expect(page).not_to have_content 'タイトル３'
+        end
+      end
+      context '左右の検索ボックスに日付を入力した場合' do
+        before do
+          fill_in 'q_event[held_on_gteq]', with: '002022-01-5'
+          fill_in 'q_event[held_on_lteq]', with: '002022-01-15'
+          find(".search_form .search_btn").click 
+        end
+        it 'その期間内のイベントのみ表示される' do
+          expect(page).not_to have_content 'タイトル１'
+          expect(page).to have_content 'タイトル２'
+          expect(page).not_to have_content 'タイトル３'
+        end
+      end
+    end
+    context 'マイページ' do
+      before do
+        travel_to Time.zone.local(2022, 1, 3)
+        FactoryBot.create(:join, user_id: user.id, event_id: event.id)
+        FactoryBot.create(:join, user_id: user.id, event_id: event_second.id)
+        FactoryBot.create(:join, user_id: user.id, event_id: event_third.id)
+        sign_in user
+        visit user_path(user)
+      end
+      context '左の検索ボックスに日付を入力した場合' do
+        before do
+          fill_in 'date_search[held_on_gteq]', with: '002022-01-10'
+          find(".search_form .search_btn").click 
+        end
+        it 'その日付以降のイベントのみ表示される' do
+          expect(page).not_to have_content 'タイトル１'
+          expect(page).to have_content 'タイトル２'
+          expect(page).to have_content 'タイトル３'
+        end
+      end
+      context '右の検索ボックスに日付を入力した場合' do
+        before do
+          fill_in 'date_search[held_on_lteq]', with: '002022-01-10'
+          find(".search_form .search_btn").click 
+        end
+        it 'その日付以前のイベントのみ表示される' do
+          expect(page).to have_content 'タイトル１'
+          expect(page).to have_content 'タイトル２'
+          expect(page).not_to have_content 'タイトル３'
+        end
+      end
+      context '左右の検索ボックスに日付を入力した場合' do
+        before do
+          fill_in 'date_search[held_on_gteq]', with: '002022-01-5'
+          fill_in 'date_search[held_on_lteq]', with: '002022-01-15'
+          find(".search_form .search_btn").click 
+        end
+        it 'その期間内のイベントのみ表示される' do
+          expect(page).not_to have_content 'タイトル１'
+          expect(page).to have_content 'タイトル２'
+          expect(page).not_to have_content 'タイトル３'
+        end
       end
     end
   end
