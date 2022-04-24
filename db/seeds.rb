741c1a2071ca80ff0be4581d7db67331d197d3d6
Miyauchi <hy.miyauchi@gmail.com>
2022-04-24 15:07:41 +0900

    [fix]seed:データ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,27 @@
 @livehouses =
 Livehouse.create!([
                     {
+                      name: 'Spotify O-nest',
+                      address: '東京都渋谷区円山町2-3 6F',
+                      url: 'https://shibuya-o.com/nest/',
+                      nearest_station: '渋谷駅より徒歩5分',
+                      place_id: '14'
+                    },
+                    {
+                      name: 'shibuya eggman',
+                      address: '東京都渋谷区神南1-6-8',
+                      url: 'http://eggman.jp/',
+                      nearest_station: '渋谷駅から徒歩10分',
+                      place_id: '14'
+                    },
+                    {
+                      name: '渋谷CLUB QUATTRO',
+                      address: '東京都渋谷区宇田川町32-13 クアトロ･パルコ4･5F',
+                      url: 'http://www.club-quattro.com/',
+                      nearest_station: '渋谷駅から徒歩7分',
+                      place_id: '14'
+                    },
+                    {
                       name: '下北沢BASEMENT BAR',
                       address: '東京都世田谷区代沢5-18-1 カラバッシュビルB1F',
                       url: 'https://toos.co.jp/basementbar/',
@@ -15,26 +36,55 @@ Livehouse.create!([
                       nearest_station: '下北沢駅より徒歩５分',
                       place_id: '15'
                     },
-                    {
-                      name: '新代田FEVER',
-                      address: '東京都世田谷区羽根木1-1-14 新代田ビル1F',
-                      url: 'https://www.fever-popo.com/',
-                      nearest_station: '新代田駅より徒歩30秒',
-                      place_id: '15'
-                    },
-                    {
-                      name: 'Spotify O-nest',
-                      address: '東京都渋谷区円山町2-3 6F',
-                      url: 'https://shibuya-o.com/nest/',
-                      nearest_station: '渋谷駅より徒歩5分',
-                      place_id: '14'
-                    },
-                    {
-                      name: 'Live House Pangea',
-                      address: '大阪市中央区西心斎橋2-10-34 心斎橋ウエスト363ビル1F',
-                      url: 'https://livepangea.com/',
-                      nearest_station: '四ツ橋駅より徒歩3分'
-                    }
+                    # {
+                    #   name: '新代田FEVER',
+                    #   address: '東京都世田谷区羽根木1-1-14 新代田ビル1F',
+                    #   url: 'https://www.fever-popo.com/',
+                    #   nearest_station: '新代田駅より徒歩30秒',
+                    #   place_id: '15'
+                    # },
+                    # {
+                    #   name: '新宿LOFT',
+                    #   address: '東京都新宿区歌舞伎町1-12-9 タテハナビルB2',
+                    #   url: 'http://www.loft-prj.co.jp/LOFT/',
+                    #   nearest_station: '新宿駅東口より徒歩10分',
+                    #   place_id: '16'
+                    # },
+                    # {
+                    #   name: 'Live House Marble',
+                    #   address: '東京都新宿区歌舞伎町2-45-2 ジャストビルB1F',
+                    #   url: 'http://marble-web.jp/html/map.html',
+                    #   nearest_station: '新宿駅東口より徒歩5分',
+                    #   place_id: '16'
+                    # },
+                    # {
+                    #   name: '高田馬場CLUB PHASE',
+                    #   address: '東京都豊島区高田3-8-5 セントラルワセダB1F',
+                    #   url: 'http://www.club-phase.com/top.html',
+                    #   nearest_station: '高田馬場駅より徒歩5分',
+                    #   place_id: '17'
+                    # },
+                    # {
+                    #   name: '池袋LiveGarage Adm',
+                    #   address: '東京都豊島区東池袋1-22-1 GSハイムB1F',
+                    #   url: 'http://www.adm-rock.com/index03.html',
+                    #   nearest_station: '池袋駅東口より徒歩10分',
+                    #   place_id: '17'
+                    # },
+                    # {
+                    #   name: '池袋LiveGarage Adm',
+                    #   address: '東京都豊島区東池袋1-22-1 GSハイムB1F',
+                    #   url: 'http://www.adm-rock.com/index03.html',
+                    #   nearest_station: '池袋駅東口より徒歩10分',
+                    #   place_id: '18'
+                    # },
+                    # {
+                    #   name: 'Live House Pangea',
+                    #   address: '大阪市中央区西心斎橋2-10-34 心斎橋ウエスト363ビル1F',
+                    #   url: 'https://livepangea.com/',
+                    #   nearest_station: '四ツ橋駅より徒歩3分',
+                    #   place_id: '42'
+                    # }
                   ])
 
 # users
@@ -65,7 +115,7 @@ end
       url: 'https://diveintocode.jp/',
       livehouse_id: livehouse.id
     )
-end
+  end
 end
 
 # blogs
