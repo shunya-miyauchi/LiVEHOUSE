258320291096e0f28b320fe197d26b66eb6e01ff
Miyauchi <hy.miyauchi@gmail.com>
2022-04-27 16:09:20 +0900

    [fix]controller:お気に入りページ検索機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,14 @@
 class FavoritesController < ApplicationController
   before_action :set_livehouse, only: %i[create destroy]
+  before_action :set_livehouses, only: %i[index]
+  before_action :q_event, only: %i[index]
 
   def index
     @livehouses = current_user.favorite_livehouses
-    @events = Event.where(livehouse_id: @livehouses.ids).includes(:livehouse).date_after_today.sort_held_on.page(params[:page]).per(12)
-    return unless request.xhr?
-
-    render :schedule
+    respond_to do |format|
+      format.html {}
+      format.js { render :schedule }
+    end
   end
 
   def create
@@ -21,5 +23,20 @@ class FavoritesController < ApplicationController
 
   def set_livehouse
     @livehouse = Livehouse.find(params[:id])
+  end
+
+  def set_livehouses
+    @livehouses = current_user.favorite_livehouses
+  end
+
+  # 期間検索
+  def q_event
+    @q_event = Event.where(livehouse_id: @livehouses.ids).includes(:livehouse).ransack(params[:date_search], search_key: :date_search)
+    @events =
+      if @q_event.conditions.present?
+        @q_event.result(distinct: true).sort_held_on.page(params[:page]).per(12)
+      else
+        Event.where(livehouse_id: @livehouses.ids).includes(:livehouse).date_after_today.sort_held_on.page(params[:page]).per(12)
+      end
   end
 end
